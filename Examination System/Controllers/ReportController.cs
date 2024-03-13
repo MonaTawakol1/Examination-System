a5ba9b536f53000bbb4b2c38de18c83cf1e1aee3
Salma <salma@gmail.com>
2024-03-13 21:07:07 +0200

    reports

---
+++ Examination System/Controllers/ReportController.cs
@@ -0,0 +1,36 @@
+﻿using Examination_System.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examination_System.Controllers
+{
+    public class ReportController : Controller
+    {
+        IReportRepo reportRepo;
+
+        public ReportController(IReportRepo _reportRepo)
+        {
+            reportRepo = _reportRepo;
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StudentsInformation()
+        {
+            var depts = reportRepo.GetDepartments();
+            ViewBag.depts = depts;
+            return View();
+        }
+
+      
+        public IActionResult Reports1(int DeptNumber)
+        {
+           var depts =  reportRepo.GetDepartments();
+            ViewBag.depts = depts;
+          var stdList =   reportRepo.StudentsInformation(DeptNumber);
+            return View(stdList);
+        }
+    }
+}
---
+++ Examination System/Repository/ReportRepo.cs
@@ -0,0 +1,32 @@
+﻿using Examination_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examination_System.Repository
+{
+
+    public interface IReportRepo
+    {
+        public List<Student> StudentsInformation(int DeptNumber);
+        public List<Department> GetDepartments();
+    }
+    public class ReportRepo: IReportRepo
+    {
+
+        ItiContext db;
+
+        public ReportRepo(ItiContext _db)
+        {
+            db = _db;
+        }
+        
+        public List<Student> StudentsInformation(int DeptNumber)
+        {
+            return  db.Students.Include(a=>a.User).Where(a=>a.DepartmentId == DeptNumber).ToList();
+        }
+        public List<Department> GetDepartments()
+        {
+            return db.Departments.ToList();
+        }
+
+    }
+}
